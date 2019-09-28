8439f9486cd8a5be6e364cb22303f62bfc568cae
IFOP0112003 Кустов Дмитрий Вячеславович <IFOP0112003@sibupk.su>
2019-09-28 12:01:58 +0700

    1

--- WPFBibleThump/ViewModel/IssuingBooksViewModel.cs
+++ WPFBibleThump/ViewModel/IssuingBooksViewModel.cs
@@ -6,16 +6,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using WPFBibleThump.Model;
 
 namespace WPFBibleThump.ViewModel
 {
     class IssuingBooksViewModel
     {
-        public ICollectionView IssuingBooks { get; set; }
+        private string _searchText;
+        public ICollectionView Readers { get; set; }
         public IssuingBooksViewModel()
         {
-            App.MOYABAZA.Выданные_книги.Load();
-            IssuingBooks = CollectionViewSource.GetDefaultView(App.MOYABAZA.Выданные_книги.Local);
+            App.MOYABAZA.Читатели.Load();
+            Readers = CollectionViewSource.GetDefaultView(App.MOYABAZA.Читатели.Local);
+            Readers.Filter = FilterFunction;
+        }
+
+        public string SearchText
+        {
+            get => _searchText;
+            set
+            {
+                _searchText = value;
+                Readers.Refresh();
+            }
+        }
+
+        bool FilterFunction(object o)
+        {
+            Читатели chitateli = o as Читатели;
+            if (String.IsNullOrEmpty(SearchText) ||
+                chitateli.Номер_читательского_билета.StartsWith(SearchText.Trim(), StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
