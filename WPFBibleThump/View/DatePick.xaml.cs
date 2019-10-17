815823f5cd96baa392db70cff4fd223e1ae0e995
IFOP0112003 Кустов Дмитрий Вячеславович <IFOP0112003@sibupk.su>
2019-10-17 10:01:33 +0700

    Добавил папку для View

--- WPFBibleThump/View/DatePick.xaml.cs
+++ WPFBibleThump/View/DatePick.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WPFBibleThump.Model;
+using WPFBibleThump.ViewModel;
 
 namespace WPFBibleThump
 {
@@ -19,9 +21,10 @@ namespace WPFBibleThump
     /// </summary>
     public partial class DatePick : Window
     {
-        public DatePick()
+        public DatePick(Читатели reader)
         {
             InitializeComponent();
+            DataContext = new IssuingBooksViewModel(reader);
         }
     }
 }
